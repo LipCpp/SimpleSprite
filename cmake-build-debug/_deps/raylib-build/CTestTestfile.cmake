d3d44fe6d6381ba3c375b017c6026dd8cbac0dc5
Lip <101120558+LipCpp@users.noreply.github.com>
2024-05-18 01:27:11 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyperion/CLionProjects/SimpleSprite/cmake-build-debug/_deps/raylib-src
-# Build directory: /home/hyperion/CLionProjects/SimpleSprite/cmake-build-debug/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
